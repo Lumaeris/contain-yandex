abc57f1daad316a00f4db5147cb193cd590ceda8
Österreich Luminota <lumaeris@proton.me>
2023-06-07 21:09:37 +0700

    Initial

--- README.md
+++ README.md
@@ -1,47 +1,43 @@
-# Google Container
+# Yandex Container
 
-**Prevent Google from tracking your visits to other websites**
+**Prevent Yandex from tracking your visits to other websites**
 
-This is a fork of [Facebook Container](https://github.com/mozilla/contain-facebook)
+This is a fork of [Google Container](https://github.com/containers-everywhere/contain-google) which itself is a fork of [Facebook Container](https://github.com/mozilla/contain-facebook)
 
-Google Container is an add-on you can install on Firefox to prevent Google from tracking your activity on other websites, so you can continue to use Google while protecting your privacy.
+Yandex Container is an add-on you can install on Firefox to prevent Yandex from tracking your activity on other websites, so you can continue to use Yandex while protecting your privacy.
 
 **Note:** To learn more about Containers in general, see [Firefox Multi-Account Containers](https://support.mozilla.org/kb/containers).
 
-## How does Google Container work?
+## How does Yandex Container work?
 
-The Add-on keeps Google in a separate Container to prevent it from following your activity on other websites. When you first install the add-on, it signs you out of Google and deletes the cookies that Google uses to track you on other websites. 
+The Add-on keeps Yandex in a separate Container to prevent it from following your activity on other websites. When you first install the add-on, it signs you out of Yandex and deletes the cookies that Yandex uses to track you on other websites. 
 
-Every time you visit Google, it will open in its own container, separate from other websites you visit.  You can login to Google within its container.  When browsing outside the container, Google won’t be able to easily collect your browsing data and connect it to your Google identity.
+Every time you visit Yandex, it will open in its own container, separate from other websites you visit.  You can login to Yandex within its container.  When browsing outside the container, Yandex won’t be able to easily collect your browsing data and connect it to your Yandex identity.
 
-## How do I enable Google Container?
+## How do I enable Yandex Container?
 
 We’ve made it easy to take steps to protect your privacy so you can go on with your day.
 
-1. [Install Google Container](https://addons.mozilla.org/firefox/addon/google-container/). This will log you out of Google and delete the cookies it’s been using to track you.
-2. Open Google and use it like you normally would.  Firefox will automatically switch to the Google Container tab for you.
-3. If you click on a link to a page outside of Google or type in another website in the address bar, Firefox will load them outside of the Google Container
+1. [Install Yandex Container](https://addons.mozilla.org/firefox/addon/yandex-container/). This will log you out of Yandex and delete the cookies it’s been using to track you.
+2. Open Yandex and use it like you normally would.  Firefox will automatically switch to the Yandex Container tab for you.
+3. If you click on a link to a page outside of Yandex or type in another website in the address bar, Firefox will load them outside of the Yandex Container
 
-## How does this affect Google’s features?
+## How does this affect Yandex’s features?
 
-Google Containers prevents Google from linking your activity on other websites to your Google identity. Therefore, the following will not work:
+Yandex Containers prevents Yandex from linking your activity on other websites to your Yandex identity. Therefore, the following will not work:
 
-### “Like” buttons and embedded Google comments on other websites.
+### Logging in or creating accounts on other websites using Yandex
 
-Because you are logged into Google only in the Container, “Like” buttons and embedded Google comments on other websites will not work.
+Websites that allow you to create an account or log in using Yandex will generally not work properly.
 
-### Logging in or creating accounts on other websites using Google
+## Will this protect me from Yandex completely?
 
-Websites that allow you to create an account or log in using Google will generally not work properly.
+This add-on does not prevent Yandex from mishandling the data it already has or permitted others to obtain about you. Yandex still will have access to everything that you do while you are on yandex.com or on any Yandex app, including your Yandex comments, photo uploads, and any data you share with Yandex connected apps, etc.  
 
-## Will this protect me from Google completely?
-
-This add-on does not prevent Google from mishandling the data it already has or permitted others to obtain about you. Google still will have access to everything that you do while you are on google.com or on any Google app, including your Google comments, photo uploads, likes, and any data you share with Google connected apps, etc.  
-
-Other ad networks may try to link your Google activities with your regular browsing. In addition to this add-on, there are other things you can do to maximize your protection, including changing your Google settings, using Private Browsing and Tracking Protection, blocking third-party cookies, and/or using [Firefox Multi-Account Containers](https://addons.mozilla.org/firefox/addon/multi-account-containers/ ) extension to further limit tracking.
+Other ad networks may try to link your Yandex activities with your regular browsing. In addition to this add-on, there are other things you can do to maximize your protection, including changing your Yandex settings, using Private Browsing and Tracking Protection, blocking third-party cookies, and/or using [Firefox Multi-Account Containers](https://addons.mozilla.org/firefox/addon/multi-account-containers/ ) extension to further limit tracking.
 
 ## How do I use Containers for other websites?
 
-Good news! Containers aren’t just for Google. You can use Containers to prevent websites from linking your identities across the Web by installing [Firefox Multi-Account Containers](https://addons.mozilla.org/firefox/addon/multi-account-containers/).
+Good news! Containers aren’t just for Yandex. You can use Containers to prevent websites from linking your identities across the Web by installing [Firefox Multi-Account Containers](https://addons.mozilla.org/firefox/addon/multi-account-containers/).
 
 To learn more about how Mult-Account Containers work, see our support page at [Firefox Multi-Account Containers](https://addons.mozilla.org/firefox/addon/multi-account-containers/).
--- background.js
+++ background.js
@@ -1,58 +1,44 @@
 // Param values from https://developer.mozilla.org/Add-ons/WebExtensions/API/contextualIdentities/create
-const GOOGLE_CONTAINER_NAME = "Google";
-const GOOGLE_CONTAINER_COLOR = "red";
-const GOOGLE_CONTAINER_ICON = "briefcase";
+const YANDEX_CONTAINER_NAME = "Yandex";
+const YANDEX_CONTAINER_COLOR = "red";
+const YANDEX_CONTAINER_ICON = "briefcase";
 
-let GOOGLE_DOMAINS = [
-  "google.com", "google.org", "googleapis.com", "g.co", "ggpht.com", "gle",
-  "blogger.com", "googleblog.com", "blog.google", "googleusercontent.com", "googlesource.com",
-  "google.org", "google.net", "466453.com", "gooogle.com", "gogle.com", "ggoogle.com", "gogole.com", "goolge.com", "googel.com", "googlee.com", "googil.com", "googlr.com", "elgoog.im", "ai.google", "com.google", "about.google", "registry.google", "google", "gstatic.com", "goog", "googleplex.com"
+let YANDEX_DOMAINS = [
+  "ya.ru", "yandex.ru", "yandex.net", "yastatic.net", "naydex.net", "yandexcloud.net", "static-storage.net", "yastat.net", "z5h64q92x9.net", "ir.yandex", "yandex-team.ru", "yandex-team.com", "yandex.com.ru", "clstorage.net", "yandex.rs", "ya.rs"
 ];
 
-const GOOGLE_INTL_DOMAINS = [
-  "google.ac", "google.ad", "google.ae", "google.com.af", "google.com.ag", "google.com.ai", "google.al", "google.am", "google.co.ao", "google.com.ar", "google.as", "google.at", "google.com.au", "google.az", "google.ba", "google.com.bd", "google.be", "google.bf", "google.bg", "google.com.bh", "google.bi", "google.bj", "google.com.bn", "google.com.bo", "google.com.br", "google.bs", "google.bt", "google.com.bw", "google.by", "google.com.bz", "google.ca", "google.com.kh", "google.cc", "google.cd", "google.cf", "google.cat", "google.cg", "google.ch", "google.ci", "google.co.ck", "google.cl", "google.cm", "google.cn", "google.com.co", "google.co.cr", "google.com.cu", "google.cv", "google.com.cy", "google.cz", "google.de", "google.dj", "google.dk", "google.dm", "google.com.do", "google.dz", "google.com.ec", "google.ee", "google.com.eg", "google.es", "google.com.et", "google.fi", "google.com.fj", "google.fm", "google.fr", "google.ga", "google.ge", "google.gf", "google.gg", "google.com.gh", "google.com.gi", "google.gl", "google.gm", "google.gp", "google.gr", "google.com.gt", "google.gy", "google.com.hk", "google.hn", "google.hr", "google.ht", "google.hu", "google.co.id", "google.iq", "google.ie", "google.co.il", "google.im", "google.co.in", "google.io", "google.is", "google.it", "google.je", "google.com.jm", "google.jo", "google.co.jp", "google.co.ke", "google.ki", "google.kg", "google.co.kr", "google.com.kw", "google.kz", "google.la", "google.lb", "google.com.lc", "google.li", "google.lk", "google.co.ls", "google.lt", "google.lu", "google.lv", "google.com.ly", "google.co.ma", "google.md", "google.me", "google.mg", "google.mk", "google.ml", "google.com.mm", "google.mn", "google.ms", "google.com.mt", "google.mu", "google.mv", "google.mw", "google.com.mx", "google.com.my", "google.co.mz", "google.com.na", "google.ne", "google.com.nf", "google.com.ng", "google.com.ni", "google.nl", "google.no", "google.com.np", "google.nr", "google.nu", "google.co.nz", "google.com.om", "google.com.pk", "google.com.pa", "google.com.pe", "google.com.ph", "google.pl", "google.com.pg", "google.pn", "google.com.pr", "google.ps", "google.pt", "google.com.py", "google.com.qa", "google.ro", "google.rs", "google.ru", "google.rw", "google.com.sa", "google.com.sb", "google.sc", "google.se", "google.com.sg", "google.sh", "google.si", "google.sk", "google.com.sl", "google.sn", "google.sm", "google.so", "google.st", "google.sr", "google.com.sv", "google.td", "google.tg", "google.co.th", "google.com.tj", "google.tk", "google.tl", "google.tm", "google.to", "google.tn", "google.com.tr", "google.tt", "google.com.tw", "google.co.tz", "google.com.ua", "google.co.ug", "google.co.uk", "google.us", "google.com.uy", "google.co.uz", "google.com.vc", "google.co.ve", "google.vg", "google.co.vi", "google.com.vn", "google.vu", "google.ws", "google.co.za", "google.co.zm", "google.co.zw",
+const YANDEX_INTL_DOMAINS = [
+  "yandex.com", "yandex.eu", "yandex.kz", "yandex.com.tr", "yandex.ua", "yandex.by", "yandex.uz"
 ];
 
-const GOOGLE_SERVICES = [
-"like.com", "keyhole.com", "panoramio.com", "picasa.com", "urchin.com", "igoogle.com", "foofle.com", "froogle.com", "localguidesconnect.com", "googlemail.com", "googleanalytics.com", "google-analytics.com", "googletagmanager.com", "googlecode.com", "googlesource.com", "googledrive.com", "googlearth.com", "googleearth.com", "googlemaps.com", "googlepagecreator.com", "googlescholar.com", "advertisercommunity.com", "thinkwithgoogle.com", "googlegroups.com",
+const YANDEX_SERVICES = [
+  "meteum.ai", "turbopages.org", "kinopoisk.ru", "thequestion.ru", "thequestion.com", "praktikum.blog", "eda.rest", "yaconnect.com", "yaconnect.ru", "yandexdatafactory.com", "yandexdatafactory.ru", "yandex-launcher.com", "yandexlauncher.com", "catboost.org", "catboost.ai", "catboost.yandex", "drive.yandex", "yandex.travel", "yandex.video", "yadi.sk", "split.ru", "yandex.market", "auto.ru", "avto.ru", "delivery-club.ru", "bookmate.ru", "bookmate.com", "edadeal.ru", "edadeal.io", "go.yandex", "auto.yandex", "toloka.ai", "yandex-bank.net"
 ];
 
-const YOUTUBE_DOMAINS = [
-  "youtube.com", "youtu.be", "yt.be", "ytimg.com", "youtube-nocookie.com", "youtubegaming.com", "youtubeeducation.com", "youtube",
-];
-
-const BLOGSPOT_DOMAINS = [
-  "blogspot.com", "blogspot.ae", "blogspot.al", "blogspot.am", "blogspot.com.ar", "blogspot.co.at", "blogspot.com.au", "blogspot.ba", "blogspot.be", "blogspot.bg", "blogspot.bj", "blogspot.com.br", "blogspot.com.by", "blogspot.ca", "blogspot.cf", "blogspot.ch", "blogspot.cl", "blogspot.com.co", "blogspot.cv", "blogspot.com.cy", "blogspot.cz", "blogspot.de", "blogspot.dj", "blogspot.dk", "blogspot.dm", "blogspot.com.do", "blogspot.dz", "blogspot.com.eg", "blogspot.es", "blogspot.fi", "blogspot.fr", "blogspot.gr", "blogspot.hr", "blogspot.hu", "blogspot.co.id", "blogspot.ie", "blogspot.co.il", "blogspot.in", "blogspot.is", "blogspot.it", "blogspot.jp", "blogspot.co.ke", "blogspot.kr", "blogspot.li", "blogspot.lt", "blogspot.lu", "blogspot.md", "blogspot.mk", "blogspot.com.mt", "blogspot.mx", "blogspot.my", "blogspot.com.ng", "blogspot.nl", "blogspot.no", "blogspot.co.nz", "blogspot.pt", "blogspot.qa", "blogspot.ro", "blogspot.rs", "blogspot.ru", "blogspot.se", "blogspot.sg", "blogspot.si", "blogspot.sk", "blogspot.sn", "blogspot.com.sr", "blogspot.td", "blogspot.co.tl", "blogspot.co.to", "blogspot.com.tr", "blogspot.tw", "blogspot.co.uk", "blogspot.com.uy", "blogspot.co.za", "stackdriver.com",
-];
-
-const ALPHABET_DOMAINS = [
-  "abc.xyz", "waze.com", "capitalg.com", "gv.com", "calicolabs.com", "x.company", "nest.com", "sidewalklabs.com", "verily.com",
+const INTERTECH_DOMAINS = [
+  "nocord-tools.ru", "tuvio.ru", "junion.ru", "commo.ru", "reverbrain.com", "statad.ru", "scantobuy.ru"
 ];
 
 const AD_DOMAINS = [
-  "doubleclickbygoogle.com", "feedburner.com", "doubleclick.com", "doubleclick.net", "adwords.com", "adsense.com", "admob.com", "advertisercommunity.com",
-  "googlesyndication.com", "googlecommerce.com", "googlebot.com", "googleapps.com", "googleadservices.com", "gmodules.com", "googl.com",
-  "1e100.net", "domains.google", "gv.com", "googletraveladservices.com",
+  "adriver.ru", "clck.yandex.ru", "adfstat.yandex.ru", "an.yandex.ru", "market-click2.yandex.ru", "log.strm.yandex.ru", "appmetrica.com", "appmetrica.ru", "csp.yandex.net", "static-mon.yandex.net", "adfox.ru", "yandex.st", "metrika.yandex.ru", "appmetrica.yandex.ru", "metrika.yandex"
 ];
 
-const DEVELOPER_DOMAINS = [
-  "madewithcode.com", "design.google", "wellbeing.google", "gallery.io", "domains.google", "material.io", "getmdl.io", "android.com", "chromium.org", "cobrasearch.com", "chromecast.com", "chrome.com", "chromebook.com", "madewithcode.com", "whatbrowser.org", "withgoogle.com", "web.dev", "google.dev", "pub.dev", "flutter.dev", "dart.dev", "grasshopper.app",
+const ZEN_DOMAINS = [
+  "dzen.ru", "dzeninfra.ru"
 ];
 
-GOOGLE_DOMAINS = GOOGLE_DOMAINS.concat(GOOGLE_INTL_DOMAINS)
-  .concat(GOOGLE_SERVICES).concat(YOUTUBE_DOMAINS).concat(BLOGSPOT_DOMAINS).concat(ALPHABET_DOMAINS)
-  .concat(DEVELOPER_DOMAINS).concat(AD_DOMAINS);
+YANDEX_DOMAINS = YANDEX_DOMAINS.concat(YANDEX_INTL_DOMAINS).concat(YANDEX_SERVICES).concat(INTERTECH_DOMAINS).concat(AD_DOMAINS).concat(ZEN_DOMAINS);
 
 const MAC_ADDON_ID = "@testpilot-containers";
 
 let macAddonEnabled = false;
-let googleCookieStoreId = null;
+let yandexCookieStoreId = null;
 let extensionSettings = {};
 
 const canceledRequests = {};
 const tabsWaitingToLoad = {};
-const googleHostREs = [];
-const youtubeHostREs = [];
+const yandexHostREs = [];
+const zenHostREs = [];
 const whitelistedHostREs = [];
 const allowlistedHostREs = [];
 
@@ -150,16 +136,16 @@ function shouldCancelEarly (tab, options) {
   return false;
 }
 
-function generateGoogleHostREs () {
+function generateYandexHostREs () {
   const matchOperatorsRegex = /[|\\{}()[\]^$+*?.-]/g;
 
-  for (let googleDomain of GOOGLE_DOMAINS) {
-    googleDomain = googleDomain.replace(matchOperatorsRegex, '\\$&');
-    googleHostREs.push(new RegExp(`(^|\\.)${googleDomain}$`));
+  for (let yandexDomain of YANDEX_DOMAINS) {
+    yandexDomain = yandexDomain.replace(matchOperatorsRegex, '\\$&');
+    yandexHostREs.push(new RegExp(`(^|\\.)${yandexDomain}$`));
   }
-  for (let youtubeDomain of YOUTUBE_DOMAINS) {
-    youtubeDomain = youtubeDomain.replace(matchOperatorsRegex, '\\$&');
-    youtubeHostREs.push(new RegExp(`(^|\\.)${youtubeDomain}$`));
+  for (let zenDomain of ZEN_DOMAINS) {
+    zenDomain = zenDomain.replace(matchOperatorsRegex, '\\$&');
+    zenHostREs.push(new RegExp(`(^|\\.)${zenDomain}$`));
   }
 }
 
@@ -191,8 +177,8 @@ async function loadExtensionSettings () {
   }
 }
 
-async function clearGoogleCookies () {
-  // Clear all google cookies
+async function clearYandexCookies () {
+  // Clear all yandex cookies
   const containers = await browser.contextualIdentities.query({});
   containers.push({
     cookieStoreId: "firefox-default"
@@ -200,39 +186,39 @@ async function clearGoogleCookies () {
 
   let macAssignments = [];
   if (macAddonEnabled) {
-    const promises = GOOGLE_DOMAINS.map(async googleDomain => {
-      const assigned = await getMACAssignment(`https://${googleDomain}/`);
-      return assigned ? googleDomain : null;
+    const promises = YANDEX_DOMAINS.map(async yandexDomain => {
+      const assigned = await getMACAssignment(`https://${yandexDomain}/`);
+      return assigned ? yandexDomain : null;
     });
     macAssignments = await Promise.all(promises);
   }
 
-  GOOGLE_DOMAINS.map(async googleDomain => {
-    const googleCookieUrl = `https://${googleDomain}/`;
+  YANDEX_DOMAINS.map(async yandexDomain => {
+    const yandexCookieUrl = `https://${yandexDomain}/`;
 
-    // dont clear cookies for googleDomain if mac assigned (with or without www.)
+    // dont clear cookies for yandexDomain if mac assigned (with or without www.)
     if (macAddonEnabled &&
-        (macAssignments.includes(googleDomain) ||
-         macAssignments.includes(`www.${googleDomain}`))) {
+        (macAssignments.includes(yandexDomain) ||
+         macAssignments.includes(`www.${yandexDomain}`))) {
       return;
     }
 
     containers.map(async container => {
       const storeId = container.cookieStoreId;
-      if (storeId === googleCookieStoreId) {
-        // Don't clear cookies in the Google Container
+      if (storeId === yandexCookieStoreId) {
+        // Don't clear cookies in the Yandex Container
         return;
       }
 
       const cookies = await browser.cookies.getAll({
-        domain: googleDomain,
+        domain: yandexDomain,
         storeId
       });
 
       cookies.map(cookie => {
         browser.cookies.remove({
           name: cookie.name,
-          url: googleCookieUrl,
+          url: yandexCookieUrl,
           storeId
         });
       });
@@ -241,17 +227,17 @@ async function clearGoogleCookies () {
 }
 
 async function setupContainer () {
-  // Use existing Google container, or create one
-  const contexts = await browser.contextualIdentities.query({name: GOOGLE_CONTAINER_NAME});
+  // Use existing Yandex container, or create one
+  const contexts = await browser.contextualIdentities.query({name: YANDEX_CONTAINER_NAME});
   if (contexts.length > 0) {
-    googleCookieStoreId = contexts[0].cookieStoreId;
+    yandexCookieStoreId = contexts[0].cookieStoreId;
   } else {
     const context = await browser.contextualIdentities.create({
-      name: GOOGLE_CONTAINER_NAME,
-      color: GOOGLE_CONTAINER_COLOR,
-      icon: GOOGLE_CONTAINER_ICON
+      name: YANDEX_CONTAINER_NAME,
+      color: YANDEX_CONTAINER_COLOR,
+      icon: YANDEX_CONTAINER_ICON
     });
-    googleCookieStoreId = context.cookieStoreId;
+    yandexCookieStoreId = context.cookieStoreId;
   }
 }
 
@@ -264,27 +250,17 @@ function reopenTab ({url, tab, cookieStoreId}) {
     windowId: tab.windowId,
     openerTabId: tab.openerTabId
   });
-  // We do not want to erase google container if going from
-  // google container back to default.
+  // We do not want to erase yandex container if going from
+  // yandex container back to default.
   if (!(isSearchPageURL(tab.url))) {
     browser.tabs.remove(tab.id);
   }
 }
 
-function isGoogleURL (url) {
+function isYandexURL (url) {
   const parsedUrl = new URL(url);
-  for (let googleHostRE of googleHostREs) {
-    if (googleHostRE.test(parsedUrl.hostname)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function isYouTubeURL (url) {
-  const parsedUrl = new URL(url);
-  for (let youtubeHostRE of youtubeHostREs) {
-    if (youtubeHostRE.test(parsedUrl.hostname)) {
+  for (let yandexHostRE of yandexHostREs) {
+    if (yandexHostRE.test(parsedUrl.hostname)) {
       return true;
     }
   }
@@ -318,14 +294,9 @@ function isSearchPageURL (url) {
   return parsedUrl.pathname.startsWith('/search');
 }
 
-function isPrefPageURL (url) {
+function isTunePageURL (url) {
   const parsedUrl = new URL(url);
-  return parsedUrl.pathname.startsWith('/preferences');
-}
-
-function isSetprefsPageURL (url) {
-  const parsedUrl = new URL(url);
-  return parsedUrl.pathname.startsWith('/setprefs');
+  return parsedUrl.pathname.startsWith('/tune');
 }
 
 function isMapsURL (url) {
@@ -333,9 +304,14 @@ function isMapsURL (url) {
   return parsedUrl.pathname.startsWith('/maps');
 }
 
-function isFlightsURL (url) {
+function isZenURL (url) {
   const parsedUrl = new URL(url);
-  return parsedUrl.pathname.startsWith('/flights');
+  for (let zenHostRE of zenHostREs) {
+    if (zenHostRE.test(parsedUrl.hostname)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function shouldContainInto (url, tab) {
@@ -344,13 +320,9 @@ function shouldContainInto (url, tab) {
     return false;
   }
 
-  let handleUrl = isGoogleURL(url) || (extensionSettings.allowlist.length!=0 && isAllowlistedURL(url));
+  let handleUrl = isYandexURL(url) || (extensionSettings.allowlist.length!=0 && isAllowlistedURL(url));
 
   if (handleUrl && extensionSettings.whitelist.length!=0 && isWhitelistedURL(url)) {
-    handleUrl = false;
-  }
-
-  if (handleUrl && extensionSettings.ignore_youtube && isYouTubeURL(url)) {
     handleUrl = false;
   }
 
@@ -358,11 +330,7 @@ function shouldContainInto (url, tab) {
     handleUrl = false;
   }
 
-  if (handleUrl && extensionSettings.ignore_prefpages && isPrefPageURL(url)) {
-    handleUrl = false;
-  }
-
-  if (handleUrl && extensionSettings.ignore_prefpages && isSetprefsPageURL(url)) {
+  if (handleUrl && extensionSettings.ignore_tunepages && isTunePageURL(url)) {
     handleUrl = false;
   }
 
@@ -370,23 +338,23 @@ function shouldContainInto (url, tab) {
     handleUrl = false;
   }
 
-  if (handleUrl && extensionSettings.ignore_flights && isFlightsURL(url)) {
+  if (handleUrl && extensionSettings.ignore_zen && isZenURL(url)) {
     handleUrl = false;
   }
 
   if (handleUrl) {
-    if (tab.cookieStoreId !== googleCookieStoreId) {
+    if (tab.cookieStoreId !== yandexCookieStoreId) {
       if (tab.cookieStoreId !== "firefox-default" && extensionSettings.dont_override_containers) {
         // Tab is already in a container, the user doesn't want us to override containers
         return false;
       }
 
-      // Google-URL outside of Google Container Tab
-      // Should contain into Google Container
-      return googleCookieStoreId;
+      // Yandex-URL outside of Yandex Container Tab
+      // Should contain into Yandex Container
+      return yandexCookieStoreId;
     }
-  } else if (tab.cookieStoreId === googleCookieStoreId) {
-    // Non-Google-URL inside Google Container Tab
+  } else if (tab.cookieStoreId === yandexCookieStoreId) {
+    // Non-Yandex-URL inside Yandex Container Tab
     // Should contain into Default Container
     return "firefox-default";
   }
@@ -456,8 +424,8 @@ async function maybeReopenAlreadyOpenTabs () {
   });
 }
 
-async function containGoogle (options) {
-  // Listen to requests and open Google into its Container,
+async function containYandex (options) {
+  // Listen to requests and open Yandex into its Container,
   // open other sites into the default tab context
   if (options.tabId === -1) {
     // Request doesn't belong to a tab
@@ -510,14 +478,14 @@ async function containGoogle (options) {
   } catch (error) {
     // TODO: Needs backup strategy
     // See https://github.com/mozilla/contain-facebook/issues/23
-    // Sometimes this add-on is installed but doesn't get a googleCookieStoreId ?
+    // Sometimes this add-on is installed but doesn't get a yandexCookieStoreId ?
     // eslint-disable-next-line no-console
     console.log(error);
     return;
   }
   loadExtensionSettings();
-  clearGoogleCookies();
-  generateGoogleHostREs();
+  clearYandexCookies();
+  generateYandexHostREs();
 
   // Clean up canceled requests
   browser.webRequest.onCompleted.addListener((options) => {
@@ -532,7 +500,7 @@ async function containGoogle (options) {
   },{urls: ["<all_urls>"], types: ["main_frame"]});
 
   // Add the request listener
-  browser.webRequest.onBeforeRequest.addListener(containGoogle, {urls: ["<all_urls>"], types: ["main_frame"]}, ["blocking"]);
+  browser.webRequest.onBeforeRequest.addListener(containYandex, {urls: ["<all_urls>"], types: ["main_frame"]}, ["blocking"]);
 
   maybeReopenAlreadyOpenTabs();
 })();
